18425cb78061b1ce7e5207801e2738ff9638ef41
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-11-16 16:21:04 +0530

    beginner real wrapper syntax

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,68 +1,23 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
-import './App.css'
-
-let counter = 0
 function App() {
-
-  const [todos , setTodos] = useState([
-    { 
-      id:1,
-      title:"go to gym", 
-      description:"go to gym 5 - 7"
-    },
-    {
-      id:2,
-      title:"go to college", 
-      description:"go to gym 10 - 2"
-    },
-    {
-      id:3,
-      title:"go to sleep", 
-      description:"go to gym sleep after dinner"
-    }
-  ])
-
-
-  function addTodo() {
-    // setTodos([...toods , {
-    //   id:4,
-    //   title:"go to dinner",
-    //   description:"go to dinner before sleep"
-    // }])
-
-
-    const newTodos = []
-    for (let i = 0; i < todos.length; i++) {
-      newTodos.push(todos[i])
-    }
-    newTodos.push({
-      id:counter++,
-      title:"go to dinner",
-      description:"go to dinner before sleep"
-    })
-    setTodos(newTodos)
-  }
-
-
   return (
     <>
       <h1>Welcome to React App</h1>
-      <button onClick={addTodo}>Add a Todo</button>
-      {todos.map((todo) => {
-        return (
-          <Todo key={todo.id} title={todo.title} description={todo.description}/>
-        )
-      })}
+      <CardWrapper innerComponent={<TextComponent title="hello"/>}/>
+      <CardWrapper innerComponent={<TextComponent title="hello guyz"/>}/>
     </>
   )
 }
 
-
-function Todo({title , description}) {
+function TextComponent({title}) {
   return <div>
-    <h1>{title}</h1>
-    <h2>{description}</h2>
+    {title}
+  </div>
+}
+
+function CardWrapper ({innerComponent}) {
+  return <div style={{ border: "2px solid black" , padding: 30}}>
+    {innerComponent}
   </div>
 }
 export default App
