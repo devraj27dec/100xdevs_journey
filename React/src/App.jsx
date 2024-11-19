403e097dccef840f2e2bfad62dc65abbcbcf4b2c
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-11-20 00:26:02 +0530

    Prop drilling or Counter Without Context Api

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,86 +1,39 @@
-// import Todos from "./assigments/Todos"
+/* eslint-disable react/prop-types */
+import { useState } from "react"
 
-// Routing
-// import {lazy , Suspense } from "react"
-// import { BrowserRouter as Router , Routes , Route, useNavigate } from "react-router-dom"
-// const Dashboard = lazy(() => import("./components/Dashboard")) 
-// const Hero = lazy(() => import("./components/Hero")) 
-
-
-// Hooks 
-// import Reconcillation from "./Hooks/Reconcillation"
-// import UseCallback from "./Hooks/UseCallback"
-// import UseRefHook from "./Hooks/useRef"
-// import UseEffecthook from "./Hooks/UseEffect"
-// import UseStateHook from "./Hooks/useStata"
-
+// Prop drilling 
 function App() {
+
+  const [count , setCount] = useState(0)
+
   return (
     <>
       <h1>Welcome to React App</h1>
-      
-      {/* Routing  */}
-      {/* <Router>
-        <AppBar/>
-        <Routes>
-          <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard/></Suspense>}/>
-          <Route path="/" element={<Suspense fallback={'loading...'}><Hero/></Suspense>}/>
-        </Routes>
-      </Router> */}
-
-
-
-
-
-
-
-
-
-      {/* <CardWrapper>
-        <TextComponent title="hi from text component 1"/>
-      </CardWrapper >
-      <CardWrapper>
-        <TextComponent title="hi from text component 2"/>
-      </CardWrapper> */}
-
-      {/* <Todos/> */}
-
-
-      {/* Hooks */}
-      {/* <UseStateHook/> */}
-      {/* <UseEffecthook/> */}
-      {/* <Reconcillation/> */}
-      {/* <UseRefHook/> */}
-
-      {/* <UseCallback/> */}
+      <div>
+        <Count count={count}/>
+        <Buttons count={count} setCount={setCount}/>
+      </div>
     </>
   )
 }
 
-// function TextComponent({title}) {
-//   return <div>
-//     {title}
-//   </div>
-// }
 
-// function CardWrapper ({children}) {
-//   return <div style={{ border: "2px solid black" , padding: 30}}>
-//     {children}
-//   </div>
-// }
+function Count({count}) {
+  return (
+    <div style={{fontSize:"30px"}}>
+      {count}
+    </div>
+  )
 
+}
 
-// function AppBar() {
-
-//   const navigate = useNavigate()
-  
-//   return (
-//     <div>
-//       <button onClick={() => navigate('/dashboard')}>Dashboard</button>
-//       <button onClick={() => navigate('/')}>Hero</button>
-//   </div>
-//   )
-// }
-
+function Buttons({count , setCount}) {
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>increase</button>
+      <button onClick={() => setCount(count - 1)}>decrease</button>
+    </div>
+  )
+}
 
 export default App
