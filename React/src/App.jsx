f3ec24f9d0646efc01d095c02bf4723387641c37
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-11-20 00:48:29 +0530

    Context Api

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,33 +1,46 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { CountContext } from "./context/CountContext"
 
-// Prop drilling 
+// Context Api -: teleport props from one componet to another without passing it down the chain
 function App() {
-
   const [count , setCount] = useState(0)
 
+  // wrap anyone that wants the teleport value inside a provider
   return (
     <>
       <h1>Welcome to React App</h1>
       <div>
-        <Count count={count}/>
-        <Buttons count={count} setCount={setCount}/>
+        <CountContext.Provider value={count}>
+          <Count setCount={setCount}/>
+        </CountContext.Provider>
       </div>
     </>
   )
 }
 
-
-function Count({count}) {
-  return (
-    <div style={{fontSize:"30px"}}>
-      {count}
-    </div>
-  )
-
+function CounterRender(){
+  let count = useContext(CountContext)
+  return <div>
+    {count}
+  </div>
 }
 
-function Buttons({count , setCount}) {
+function Count({setCount}) {
+  let count = useContext(CountContext)
+  return (
+    <>  
+    <div style={{fontSize:"30px"}}>
+      <CounterRender count={count}/>
+    </div>
+      <Buttons count={count} setCount={setCount}/>
+    </>
+    
+  )
+}
+
+function Buttons({setCount}) {
+  let count = useContext(CountContext)
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>increase</button>
