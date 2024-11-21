d12db237fbbef8f576bd1bc239ea211073eeb813
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-11-21 18:37:20 +0530

    Setup of paytm app with frontend & backend🔥🚀

---
+++ paytm/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return(
+    <div className=' text-3xl text-center p-3 text-blue-600'>
+      Welcome to Paytm wallet App
+    </div>
+  )
+}
+
+export default App
