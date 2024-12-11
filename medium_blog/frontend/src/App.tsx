36b7046a27b7fe1377d89542367a330ea2a75b5d
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-12-12 02:16:12 +0530

    frontend_setup

---
+++ medium_blog/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className=' flex text-center bg-yellow-300 text-red-900'>
+        Welcome to medium blog 
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
