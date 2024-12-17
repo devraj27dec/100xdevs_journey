38dee3ee65cb5686e50796bf9d804a0636c093c9
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-12-17 16:43:59 +0530

    Added Postgres Db with prisma

--- paytm2/apps/user-app/app/page.tsx
+++ paytm2/apps/user-app/app/page.tsx
@@ -1,3 +1,7 @@
+
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient()
+
 export default function Home() {
   return (
     <div className="flex justify-center bg-red-800 text-yellow-200">
